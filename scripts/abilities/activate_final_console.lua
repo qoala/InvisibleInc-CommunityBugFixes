358e4bc46102a1ecd650e49d935fb0bc238a9900
Qoalabear <512290+qoala@users.noreply.github.com>
2020-08-04 20:55:11 -1000

    Unbreak the ability to win the game

--- scripts/abilities/activate_final_console.lua
+++ scripts/abilities/activate_final_console.lua
@@ -5,17 +5,41 @@ local abilitydefs = include( "sim/abilitydefs" )
 
 local oldActivateFinalConsole = abilitydefs.lookupAbility("activate_final_console")
 local oldIsTarget = oldActivateFinalConsole.isTarget
+local oldAcquireTargets = oldActivateFinalConsole.acquireTargets
+
+-- Returns true if within the surrounding 8 tiles
+-- Preserves ability to "remote-hack" the final mainframe terminal anywhere along its 3-wide length.
+local function isCloseEnough( unit, targetUnit )
+    local x0, y0 = unit:getLocation()
+    local x1, y1 = targetUnit:getLocation()
+    return math.abs(x0 - x1) <= 1 and math.abs(y0 - y1) <= 1
+end
 
 local activate_final_console = util.extend(oldActivateFinalConsole)
 {
-  isTarget = function( self, abilityOwner, unit, targetUnit )
-    local sim = abilityOwner:getSim()
-    if sim:getParams().difficultyOptions.cbf_ending_remotehacking then
-      if targetUnit ~= abilityOwner then
-        return false
-      end
-    end
-    return oldIsTarget( self, abilityOwner, unit, targetUnit )
-  end
+	isTarget = function( self, abilityOwner, unit, targetUnit )
+		local sim = abilityOwner:getSim()
+		if sim:getParams().difficultyOptions.cbf_ending_remotehacking then
+			if targetUnit ~= abilityOwner then
+				return false
+			end
+		end
+		return oldIsTarget( self, abilityOwner, unit, targetUnit )
+	end,
+
+	acquireTargets = function( self, targets, game, sim, abilityOwner, unit )                                                                                                       local units = {}
+		if not sim:getParams().difficultyOptions.cbf_ending_remotehacking then
+            return oldAcquireTargets( self, targets, game, sim, abilityOwner, unit )
+		end
+
+        local units = {}
+		for _, targetUnit in pairs(sim:getAllUnits()) do
+			if self:isTarget( abilityOwner, unit, targetUnit ) and isCloseEnough( unit, targetUnit ) then
+				table.insert( units, targetUnit )
+			end
+		end
+
+		return targets.unitTarget( game, units, self, abilityOwner, unit )
+	end,
 }
 return activate_final_console
