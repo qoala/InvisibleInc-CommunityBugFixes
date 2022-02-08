3a8f9a56211600eed00e5bd6b6e35a7fe30904db
Qoalabear <512290+qoala@users.noreply.github.com>
2022-02-07 22:30:29 -0800

    Rewrite alarm patch as an append on the element's setTooltip

--- scripts/hud.lua
+++ scripts/hud.lua
@@ -12,56 +12,20 @@ function hud.createHud( ... )
 	local ALARM_TOOLTIP = string.gsub(STRINGS.UI.ALARM_TOOLTIP, "5", "{1}")
 	local ADVANCED_ALARM_TOOLTIP = string.gsub(STRINGS.UI.ADVANCED_ALARM_TOOLTIP, "5", "{1}")
 
-	local function refreshAlarmTooltip( self, trackerNumber, ... )
-		-- ====
-		-- Modified copy of vanilla calculation from refreshTrackerAdvance. Changes at CBF.
-		-- ====
-		local stage = self._game.simCore:getTrackerStage( math.min( simdefs.TRACKER_MAXCOUNT, trackerNumber ))
-		local params = self._game.params
+	local oldSetTooltip = hudObject._screen.binder.alarm.setTooltip
+	function hudObject._screen.binder.alarm:setTooltip( tip )
+		local params = hudObject._game.params
 
-		-- CBF: default to ALARM_TOOLTIP, not ADVANCED_ALARM_TOOLTIP
-		-- CBF: use paramaterized versions of the tooltips
-		local tip = util.sformat( ALARM_TOOLTIP, simdefs.TRACKER_INCREMENT )
+		-- Use ALARM_TOOLTIP when appropriate, instead of always using ADVANCED_ALARM_TOOLTIP
+		local newTip = nil
 		if params.missionEvents and params.missionEvents.advancedAlarm then
-			tip = util.sformat( ADVANCED_ALARM_TOOLTIP, simdefs.TRACKER_INCREMENT )
-		end
-
-		local alarmList = self._game.simCore:getAlarmTypes()
-		local next_alarm = simdefs.ALARM_TYPES[alarmList][stage+1]
-
-		if next_alarm then
-			tip = tip .. alarm_states.alarm_level_tips[next_alarm]
+			newTip = util.sformat( ADVANCED_ALARM_TOOLTIP, simdefs.TRACKER_INCREMENT )
 		else
-			tip = tip .. STRINGS.UI.ALARM_NEXT_AFTER_SIX
+			newTip = util.sformat( ALARM_TOOLTIP, simdefs.TRACKER_INCREMENT )
 		end
+		tip = string.gsub(tip, STRINGS.UI.ADVANCED_ALARM_TOOLTIP, newTip)
 
-		self._screen.binder.alarm:setTooltip(tip)
-	end
-
-	local oldRefreshHud = hudObject.refreshHud
-	function hudObject:refreshHud( ... )
-		oldRefreshHud( self, ... )
-		refreshAlarmTooltip( self, self._game.simCore:getTracker() )
-	end
-
-	local function noOp()
-	end
-
-	local oldOnSimEvent = hudObject.onSimEvent
-	function hudObject:onSimEvent( ev, ... )
-		local oldSetTooltip = self._screen.binder.alarm.setTooltip
-		if ev.eventType == simdefs.EV_ADVANCE_TRACKER and not ev.eventData.alarmOnly then
-			self._screen.binder.alarm.setTooltip = noOp
-		end
-
-		local result = oldOnSimEvent( self, ev, ... )
-
-		if ev.eventType == simdefs.EV_ADVANCE_TRACKER and not ev.eventData.alarmOnly then
-			self._screen.binder.alarm.setTooltip = oldSetTooltip
-			refreshAlarmTooltip( self, ev.eventData.tracker + ev.eventData.delta )
-		end
-
-		return result
+		oldSetTooltip(self, tip)
 	end
 
 	return hudObject
