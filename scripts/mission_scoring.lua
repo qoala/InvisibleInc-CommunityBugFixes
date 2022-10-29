520258fad0dd39ed027a538d8494558587de320c
Qoalabear <512290+qoala@users.noreply.github.com>
2022-10-29 12:28:44 -0700

    Prefer MIA over RESCUED if you lost the new agent

--- scripts/mission_scoring.lua
+++ scripts/mission_scoring.lua
@@ -80,17 +80,26 @@ mission_scoring.DoFinishMission = function( sim, campaign, ... )
 	-- Rescued agent status fix
 	-- -----
 	local rescued_agents = {}
+	local mia_agents = {}
 	-- Find rescued agents first
 	for i, agent in ipairs( sim._resultTable.agents ) do
-		if agent.status == "RESCUED" and not agent._cbf_name and type(agent.name) == "table" then
+		if agent.status == "RESCUED" and not agent._cbf_name and type(agent.name) == "table" and type(agent.name.template) == "string" then
 			agent.name = agent.name.template
 			agent._cbf_name = agent.name -- Fix for MAA applying this transform a second time
-			table.insert(rescued_agents, agent.name)
+			rescued_agents[agent.name] = true
+		elseif agent.status ~= "ACTIVE" and type(agent.name) == "string" then
+			-- Something *else* happened to the agent after they were rescued. That status should take precedence.
+			mia_agents[agent.name] = true
 		end
 	end
 	-- Delete the duplicates
 	for i=#sim._resultTable.agents, 1, -1 do
-		if array.find( rescued_agents, sim._resultTable.agents[i].name ) and sim._resultTable.agents[i].status ~= "RESCUED" then
+		local agent = sim._resultTable.agents[i]
+		if mia_agents[agent.name] and agent.status == "RESCUED" then
+			-- Delete RESCUED if MIA.
+			table.remove( sim._resultTable.agents, i )
+		elseif not mia_agents[agent.name] and rescued_agents[agent.name] and agent.status == "ACTIVE" then
+			-- Delete ACTIVE if RESCUED.
 			table.remove( sim._resultTable.agents, i )
 		end
 	end
