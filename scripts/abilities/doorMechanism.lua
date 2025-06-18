73187e6a1c284e5ef4400672ce3fc401668087bf
Qoalabear <512290+qoala@users.noreply.github.com>
2025-06-17 19:14:09 -0700

    Include diagonal doorMechanism use by RolandJ

--- scripts/abilities/doorMechanism.lua
+++ scripts/abilities/doorMechanism.lua
@@ -20,7 +20,16 @@ function abil:executeAbility(sim, unit, userUnit, x0, y0, dir)
         userUnit:resetAllAiming()
     end
 
-    sim:dispatchEvent(simdefs.EV_UNIT_USEDOOR, {unitID = userUnit:getID(), facing = dir})
+    -- RolandJ: If we're not on the target tile (the near side of the door),
+    -- face that tile, instead of facing the exit's dir.
+    local ux, uy = userUnit:getLocation()
+    local vizFacing
+    if ux == x0 and uy == y0 then
+        vizFacing = dir
+    else
+        vizFacing = simquery.getDirectionFromDelta(x0 - ux, y0 - uy)
+    end
+    sim:dispatchEvent(simdefs.EV_UNIT_USEDOOR, {unitID = userUnit:getID(), facing = vizFacing})
     -- Function Library: decodeVault support for NIAA
     local doorDevice = nil
     if unit:getTraits().decodeVault then
@@ -28,13 +37,15 @@ function abil:executeAbility(sim, unit, userUnit, x0, y0, dir)
     else
         doorDevice = include(string.format("sim/units/%s", unit:getTraits().doorDevice))
     end
+    -- RJ: Must use dir for the exit, not vizFacing, in order to apply device correctly.
     doorDevice.applyToDoor(sim, fromCell, dir, unit, userUnit)
     inventory.useItem(sim, userUnit, unit)
 
     sim:emitSound(simdefs.SOUND_PLACE_TRAP, x0, y0, userUnit)
 
     sim:processReactions(userUnit)
-    sim:dispatchEvent(simdefs.EV_UNIT_USEDOOR_PST, {unitID = userUnit:getID(), facing = dir})
+    -- RJ: vizFacing again.
+    sim:dispatchEvent(simdefs.EV_UNIT_USEDOOR_PST, {unitID = userUnit:getID(), facing = vizFacing})
     -- Function Library: support for LEVER
     if simquery.LEVER_resetStatus then
         simquery.LEVER_resetStatus(sim, userUnit, "ANY", false)
