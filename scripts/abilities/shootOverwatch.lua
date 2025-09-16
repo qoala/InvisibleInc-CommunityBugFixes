e78f9e43499505647d84146b59ce5772447f6a73
Phyrax <124089724+Phyrax1@users.noreply.github.com>
2025-09-16 15:19:43 +0200

    Switched bail out to onTrigger

--- scripts/abilities/shootOverwatch.lua
+++ scripts/abilities/shootOverwatch.lua
@@ -4,11 +4,16 @@
 local abilitydefs = include("sim/abilitydefs")
 local shootOverwatch = abilitydefs.lookupAbility("shootOverwatch")
 
-local origExecuteAbility = shootOverwatch.executeAbility
-function shootOverwatch:executeAbility(sim, unit, userUnit, targetUnit, ...)
+local origOnTrigger = shootOverwatch.onTrigger
+function shootOverwatch:onTrigger(sim, evType, evData, userUnit, ...)
     if userUnit:getTraits()._cbf_aim_id == sim:getActionCount() then
         return
     end
+    origOnTrigger(self, sim, evType, evData, userUnit, ...)
+end
+
+local origExecuteAbility = shootOverwatch.executeAbility
+function shootOverwatch:executeAbility(sim, unit, userUnit, targetUnit, ...)
     origExecuteAbility(self, sim, unit, userUnit, targetUnit, ...)
     sim:processReactions(userUnit) -- issue #19 
 end
